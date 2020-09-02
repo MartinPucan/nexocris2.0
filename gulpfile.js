64006c598372857e2732f9c84c6d566495ce6981
MartinPucan <pucan.m@seznam.cz>
2020-09-02 18:04:27 +0200

    remove gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-function defaultTask(cb) {
-	// place code for your default task here
-	cb();
-}
+const gulp = require("gulp");
+const sass = require('gulp-sass');
 
-exports.default = defaultTask
+gulp.task('hello', function() {
+	return new Promise(function(resolve, reject) {
+		console.log("HTTP Server Started");
+		resolve();
+	});
+});
+
+gulp.task('sass', function() {
+	return gulp.src('public/scss/app.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css'))
+});
